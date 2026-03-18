7dfee05efa5579ee70074ec775affab1e0207367
Nitish Kumar Diwakar <nitishkumardiwakar@gmail.com>
2026-03-18 15:06:34 +0530

    update

--- manage/upload.php
+++ manage/upload.php
@@ -8,10 +8,23 @@
 $baseDir = realpath(__DIR__ . '/../uploads');
 
 $dir = $_POST['dir'] ?? '';
-$targetDir = realpath($baseDir . '/' . $dir);
 
-if ($targetDir === false || strpos($targetDir, $baseDir) !== 0) {
+/* normalize directory (important for Windows + Linux) */
+$dir = str_replace(['\\', '..'], ['/', ''], $dir);
+
+/* build full path safely */
+$targetDir = $baseDir . ($dir ? DIRECTORY_SEPARATOR . $dir : '');
+
+/* resolve real path */
+$realTarget = realpath($targetDir);
+
+/* Security: prevent traversal */
+if ($realTarget === false || strpos($realTarget, $baseDir) !== 0) {
     die("Invalid directory");
+}
+
+if (!is_dir($realTarget)) {
+    die("Directory does not exist");
 }
 
 $allowed = ['jpg','jpeg','png','gif','webp'];
@@ -22,12 +35,19 @@ foreach ($_FILES['files']['tmp_name'] as $i => $tmp) {
     if (!is_uploaded_file($tmp)) continue;
 
     $name = basename($_FILES['files']['name'][$i]);
+
+    /* normalize filename */
+    $name = str_replace(['\\','/'], '', $name);
+
     $ext = strtolower(pathinfo($name, PATHINFO_EXTENSION));
 
     if (!in_array($ext, $allowed)) continue;
 
-    move_uploaded_file($tmp, $targetDir . '/' . $name);
+    $dest = $realTarget . DIRECTORY_SEPARATOR . $name;
+
+    move_uploaded_file($tmp, $dest);
 }
 
 /* redirect back */
 header("Location: index.php?dir=" . urlencode($dir));
+exit;
